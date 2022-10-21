8eb6c8129e2c9f98a8584ef72dc7886f898f928f
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2022-10-21 16:33:25 +0200

    update links style.css and script.js

--- ex_02/script.js
+++ ex_02/script.js
@@ -1,15 +1,15 @@
 let givenNumber = 0;
+let findedNumber = 0;
+let biggerNumber = 0;
+let smallerNumber = 50;
+let counter = 0;
+
 
 const askNumber = () => {
   givenNumber = parseInt(window.prompt("Choose the number to guess"));
 };
 
 askNumber();
-
-let biggerNumber = 0;
-let smallerNumber = 50;
-let counter = 0;
-let findedNumber = 0;
 
 document.addEventListener("keypress", (event) => {
   if (event.key == "Enter") {
@@ -35,15 +35,15 @@ document.getElementById("submit-number").addEventListener("click", () => {
   }
   else if (findedNumber > givenNumber) {
 
-    if(findedNumber > smallerNumber) {
-      document.getElementById("response").innerHTML = `The right number is between ${biggerNumber} and ${smallerNumber}. Stay in the range.`;
-    } 
-    else {
-      document.getElementById("smaller-number").innerHTML = findedNumber;
-      smallerNumber = findedNumber;
-      document.getElementById("response").innerHTML = "SMALLER";
-      document.getElementById("number-counter").innerHTML = counter += +1;
-    } 
+      if(findedNumber > smallerNumber) {
+        document.getElementById("response").innerHTML = `The right number is between ${biggerNumber} and ${smallerNumber}. Stay in the range.`;
+      } 
+      else {
+        document.getElementById("smaller-number").innerHTML = findedNumber;
+        smallerNumber = findedNumber;
+        document.getElementById("response").innerHTML = "SMALLER";
+        document.getElementById("number-counter").innerHTML = counter += +1;
+      } 
   }
   else if ( isNaN(findedNumber) ) {
     document.getElementById("response").innerHTML = "Please enter a number";
